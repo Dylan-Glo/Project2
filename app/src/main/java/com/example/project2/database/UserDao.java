895772ea1141bcaaa31ec8f6e6eb532a6889a33d
dylan <dylangloignacio@gmail.com>
2022-05-19 12:54:05 -0700

    Set up room database

--- app/src/main/java/com/example/project2/database/UserDao.java
+++ app/src/main/java/com/example/project2/database/UserDao.java
@@ -5,12 +5,17 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+import com.example.project2.model.User;
+
 import java.util.List;
 
 @Dao
 public interface UserDao {
     @Query("Select * from User")
     List<User> getUsersList();
+
+    @Query("select * from User where username = :username & password = :password")
+    List<User> getUserLogin(String username, String password);
 
     @Insert
     void insertUser(User user);
---
+++ app/src/main/java/com/example/project2/model/User.java
@@ -0,0 +1,46 @@
+package com.example.project2.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "User")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0;
+
+    @ColumnInfo(name = "username")
+    private String username;
+
+    @ColumnInfo(name = "password")
+    private String password;
+    
+    public User(String username, String password) {
+       this.password = password;
+       this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
